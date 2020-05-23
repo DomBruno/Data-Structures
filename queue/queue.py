b15ef4fe113c3cbbdb3cb0829a90d47d30738cf8
Dominick Bruno <dbruno@unseen.is>
2020-05-22 21:49:52 -0400

    queue.py 4/4 tests pass

--- queue/queue.py
+++ queue/queue.py
@@ -27,6 +27,12 @@ class Queue:
 
     def enqueue(self, value):
         self.storage.add_to_tail(value)
+        self.size +=1
 
     def dequeue(self):
-        return self.storage.remove_from_head()
+        if self.size == 0:
+            return
+        else:
+            self.size -=1
+            return self.storage.remove_from_head()
+            
